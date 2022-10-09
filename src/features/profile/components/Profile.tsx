31ec7c2f57da2d9904c9b2a096ae3fa21cb42a8c
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2022-10-09 18:53:12 +0900

    🔧 fix import bugs

--- src/features/profile/components/Profile.tsx
+++ src/features/profile/components/Profile.tsx
@@ -1,5 +1,4 @@
-import SelfIntroduction from './SelfIntroduction'
-
+import SelfIntroduction from '@/features/profile/components/Profile'
 import Skills from '@/features/profile/components/Skills'
 
 type Props = {
