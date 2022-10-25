f0ea359f9ebf757732c884de9e1cf7a73c99e0ea
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2022-10-25 22:42:46 +0900

    🔧 this is test

---
+++ src/pages/test-2.tsx
@@ -0,0 +1,15 @@
+import Container from '@/components/layouts/container/Container'
+import Profile from '@/features/profile/components/Profile'
+
+const ProfilePage = () => {
+  return (
+    <Container>
+      <Profile
+        title="Profile Page"
+        subtitle="Feel free to read short description about me."
+      />
+    </Container>
+  )
+}
+
+export default ProfilePage
