8867a17a7906de5fee12fbbc978e882fd6769c79
NaTsuYa♤ <65702108+natsuya-kume@users.noreply.github.com>
2022-10-26 23:05:29 +0900

    💡 Add syntax-highlighting in blog page (#28)

--- src/features/blog/components/ConvertBody.tsx
+++ src/features/blog/components/ConvertBody.tsx
@@ -1,27 +1,35 @@
-import parse, { Element } from 'html-react-parser'
-import Image from 'next/image'
+import 'highlight.js/styles/night-owl.css'
+import cheerio from 'cheerio'
+import hljs from 'highlight.js'
 
 type Props = {
   contentHTML: string
 }
 
 export const ConvertBody: React.FC<Props> = ({ contentHTML }) => {
-  const contentReact = parse(contentHTML, {
-    replace: (node) => {
-      if (node instanceof Element && node.attribs && node.name === 'img') {
-        const { src, alt, width, height } = node.attribs
-        return (
-          <Image
-            layout="responsive"
-            src={src}
-            width={width}
-            height={height}
-            alt={alt}
-            sizes="(min-width: 768px) 768px, 100vw"
-          />
-        )
-      }
-    },
+  const $ = cheerio.load(contentHTML)
+  $('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto($(elm).text())
+    $(elm).html(result.value)
+    $(elm).addClass('hljs')
   })
-  return <>{contentReact}</>
+  // {MEMO: This is used when fix image size in blog page.}
+  // const contentReact = parse($.html(), {
+  //   replace: (node) => {
+  //     if (node instanceof Element && node.attribs && node.name === 'img') {
+  //       const { src, alt, width, height } = node.attribs
+  //       return (
+  //         <Image
+  //           layout="responsive"
+  //           src={src}
+  //           width={width}
+  //           height={height}
+  //           alt={alt}
+  //           sizes="(min-width: 768px) 768px, 100vw"
+  //         />
+  //       )
+  //     }
+  //   },
+  // })
+  return <div dangerouslySetInnerHTML={{ __html: $.html() }}></div>
 }
