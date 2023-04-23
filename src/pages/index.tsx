9a5b41668f49d76a3525d0ca852b0495aeeb3bf6
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2023-04-23 17:22:50 +0900

    fix: Homeのディレクトリ構成を修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 import Container from '@/components/layouts/container/Container'
 import Meta from '@/components/layouts/meta/Meta'
-import Home from '@/features/home/components/Home'
+import Home from '@/features/home/Home'
 
 const HomePage: NextPage = () => {
   return (
