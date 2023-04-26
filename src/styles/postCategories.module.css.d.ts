5a5729390db4e5495b6c3cd57d47f96773ee13f4
NatsuyaKume <65702108+natsuya-kume@users.noreply.github.com>
2023-04-26 22:01:28 +0900

    fix: cssファイルを修正

---
+++ src/styles/postCategories.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "flexContainer": string;
+  readonly "heading": string;
+  readonly "list": string;
+  readonly "categoryTag": string;
+  readonly "categoryTagText": string;
+};
+export = styles;
+
---
+++ src/styles/privacy.module.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "flexContainer": string;
+  readonly "text": string;
+  readonly "title": string;
+  readonly "subtitle": string;
+  readonly "googleanalyticsText": string;
+};
+export = styles;
+
