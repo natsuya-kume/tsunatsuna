337dbc8184a1795104aa26430b26d1b657815aa1
Natsuya Kume <natsu.728.mic@icloud.com>
2024-01-01 18:25:01 +0900

    chore: アプリ側に通知する

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 
 import Container from '@/components/layouts/container/Container'
 import Meta from '@/components/layouts/meta/Meta'
 import Home from '@/features/home/Home'
 
 const HomePage: NextPage = () => {
+  console.log()
+
+  useEffect(() => {
+    if ('ReactNativeWebView' in window) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ;(window.ReactNativeWebView as any).postMessage(
+        JSON.stringify({ type: 'xAuthSuccess', hoge: document.location })
+      )
+    }
+  }, [])
   return (
     <Container>
       <Meta />
